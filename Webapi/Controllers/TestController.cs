c52a98cec2ec74dfb0640a9e5b5d01cfc31f6037
Maxi <herreramaxi@gmail.com>
2022-01-24 12:52:15 +0000

    creating webapi test

---
+++ Webapi/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Webapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {    
+
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello";
+        }
+    }
+}
